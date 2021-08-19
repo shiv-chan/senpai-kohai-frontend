720a7a26f00d6306d0c3cdd2d2ce2422b33c415e
shiv-chan <kshibuya31@gmail.com>
2021-08-19 10:23:28 -0700

    Update forgot password page

--- senpai-kohai/src/features/signup/ForgotPassword.jsx
+++ senpai-kohai/src/features/signup/ForgotPassword.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import mypassword from '../../assets/undraw_my_password_d6kg.svg';
 
 function ForgotPassword() {
-	return <h1>This is Forgot password page.</h1>;
+	const [email, setEmail] = useState('');
+
+	return (
+		<div className="forgotpw">
+			<article className="forgotpw-left">
+				<h2>Come Back Now!</h2>
+				<img src={mypassword} alt="mypassword" />
+				<p>Find your guide, Support youngs</p>
+			</article>
+			<article className="forgotpw-right">
+				<h1>Forgot password?</h1>
+				<form for="" method="">
+					<label for="email">Email address</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						value={email}
+						onChange={(e) => setEmail(e.currentTarget.value)}
+					/>
+					<button type="submit" onClick={(e) => e.preventDefault()}>
+						Send
+					</button>
+					<Link to="/login">Back to login</Link>
+				</form>
+			</article>
+		</div>
+	);
 }
 
 export default ForgotPassword;
